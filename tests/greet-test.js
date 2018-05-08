b04dcf1e503debb7fade5c6c5dbce701e045c8d8
Theophelus <aneletom10@gmail.com>
2018-05-08 11:00:12 +0200

    updating counter

--- tests/greet-test.js
+++ tests/greet-test.js
@@ -1,12 +1,12 @@
 describe('greeting widget', function(){
   it('should return checked greeting if English is selected', function(){
     var newGreetings = greetings();
-    newGreetings.setEnteredName('English',{'anele': 0});
-    assert.equal(1, newGreetings.getEnteredName(1));
+    //newGreetings.setEnteredName('English','anele');
+    assert.equal(newGreetings.setEnteredName('English','anele'),'Hello, anele');
   });
   // it('should return checked greeting if isiXhosa is selected', function(){
   //   var newGreetings = greetings();
-  //   newGreetings.setLanguage('IsiXhosa');
+  //   newGreetings.setLanguage('IsiXhosa',{'andrew': 0, 'yegsn'});
   //   assert.equal('Molo', newGreetings.getLanguage());
   // });
   // it('should return checked greeting if Afrikaans is selected', function(){
