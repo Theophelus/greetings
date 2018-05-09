4499640ccc43b8c9af0fb10245f4cbdd9b8b508d
Theophelus <aneletom10@gmail.com>
2018-05-09 13:59:51 +0200

    Rewriting tests for greet

--- tests/greet-test.js
+++ tests/greet-test.js
@@ -1,7 +1,46 @@
 describe('greeting widget', function(){
-  it('should return checked greeting if English is selected', function(){
+  it('should return English and the name', function(){
     var newGreetings = greetings();
-    //newGreetings.setEnteredName('English','anele');
-    assert.equal(newGreetings.setEnteredName('English','anele'),'Hello, anele');
+    assert.equal('Hello, Anele', newGreetings.setEnteredName('English','Anele'));
+  });
+  it('should return IsiXhosa and the name', function(){
+    var newGreetings = greetings();
+    assert.equal('Molo, Andrew',newGreetings.setEnteredName('IsiXhosa','Andrew'));
+  });
+  it('should return Afrikaans and the name', function(){
+    var newGreetings = greetings();
+    assert.equal('Goeie Dag, Nella', newGreetings.setEnteredName('Afrikaans','Nella'));
+  });
+  it('should be able to count two different names', function(){
+    var newGreetings = greetings();
+    newGreetings.setEnteredName('English', 'Anele');
+    newGreetings.setEnteredName('Afrikaan', 'Zintle');
+    assert.deepEqual(2, newGreetings.getEnteredName());
+  });
+  it('should count should be able to count one name once', function(){
+    var newGreetings = greetings();
+    newGreetings.setEnteredName('English', 'Anele');
+    newGreetings.setEnteredName('English', 'Anele');
+    newGreetings.setEnteredName('Afrikaan', 'Zintle');
+    assert.deepEqual(2, newGreetings.getEnteredName());
+  });
+  it('should count the number of greet in IsiXhosa if name entered', function(){
+    var newGreetings = greetings();
+    newGreetings.setEnteredName('IsiXhosa, Anele');
+    assert.deepEqual(1, newGreetings.getEnteredName());
+  });
+  it('should count the number of names regardless language selected', function(){
+    var newGreetings = greetings();
+    newGreetings.setEnteredName('English', 'Anele');
+    newGreetings.setEnteredName('IsiXhosa', 'Andrew');
+    newGreetings.setEnteredName('Afrikaan', 'Zintle');
+    assert.deepEqual(3, newGreetings.getEnteredName());
+  });
+  it('should count names in the map', function(){
+    var newGreetings = greetings();
+    newGreetings.setEnteredName('IsiXhosa', 'Anele');
+    newGreetings.setEnteredName('English', 'Ace');
+    assert.deepEqual(newGreetings.map(),{Anele: 0, Ace: 0
+    });
   });
 });
